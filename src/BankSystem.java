a1bb0b1a558a770397ecda06080e505eaf3c406a
Felipe <antoniofelipesilva@live.com>
2024-03-26 19:48:53 -0300

    resolution bug scanner int

--- src/BankSystem.java
+++ src/BankSystem.java
@@ -12,28 +12,24 @@ public class BankSystem {
         String typeAccount = "Corrente";
 
         String password = "a8f3s3f3";
-        boolean passwordAccess = false;
-
         int bankAccountNumber = 5071;
-        boolean bankAccountAccess = false;
 
         boolean accessOrBank = false;
 
+        System.out.println("enter your name:");
+        String nowName = Reading.nextLine();
+        System.out.println("enter your password");
+        String nowPassword = Reading.nextLine();
         System.out.println("enter your account:");
         int verificationBankAccountNumber = Reading.nextInt();
-        if (bankAccountNumber == verificationBankAccountNumber) {
+
+        if (bankAccountNumber == verificationBankAccountNumber && nowPassword.equals(password) ) {
             System.out.println("Account Validate");
-            bankAccountAccess = true;
+            accessOrBank = true;
         }else {
             System.out.println("Account invalidate");
         }
-        System.out.println("enter your password");
-        String nowPassword = Reading.nextLine();
-        System.out.println(nowPassword);
-
-
-
-        if (bankAccountAccess){
+        if (accessOrBank){
             System.out.println("Welcome " +firstName);
         }
     }
